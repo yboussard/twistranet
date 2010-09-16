3f6a27b40472c9536584b9c9306cca9b6e847e02
pjgrizel <pjgrizel@54d5ac7e-47a4-472c-8442-392f3838c228>
2010-09-16 07:21:10 +0000

    Better 'viewlets' management
    git-svn-id: https://svn.liances.com/svnroot/projects/TwistraNet/trunk@4060 54d5ac7e-47a4-472c-8442-392f3838c228

--- content/views.py
+++ content/views.py
@@ -20,12 +20,11 @@ def account(request, account_id):
     """
     account = Account.objects.get(id = account_id)
     latest_list = Content.secured(account).filter(account = account)
-    latest_content_viewlets = [ HtmlContentViewlet(content) for content in latest_list ]
     t = loader.get_template('account.html')
     t = RequestContext(
         request,
         {
-            "latest_content_list": latest_content_viewlets,
+            "latest_content_list": latest_list,
         },
         )
     return HttpResponse(t.render(c))
@@ -56,14 +55,13 @@ def wall(request):
 
     # Content displayed on the wall
     latest_list = Content.secured(account).all().order_by('-date')[:5]
-    latest_content_viewlets = [ HtmlContentViewlet(content) for content in latest_list ]
     
     # Render the template
     t = loader.get_template('wall.html')
     c = RequestContext(
         request,
         {
-            'latest_content_list': latest_content_viewlets,
+            'latest_content_list': latest_list,
             'form': form,
         },
         )
@@ -71,43 +69,5 @@ def wall(request):
     # Or can do:
     # return render_to_response('wall.html', {'latest_content_list': latest_content_list})
     
-    
-class HtmlContentViewlet:
-    """
-    This superclass is used to provide special "viewlets" to content templates.
-    """
-    def __init__(self, content):
-        """
-        Init the superclass with the content itself.
-        We ensure to init the proper subclass to display a nice view.
-        """
-        # Get right content type
-        target_content = getattr(content, content.content_type.lower())
-        self.content = target_content
-    
-    def htmlSingle(self, ):
-        """
-        This special view renders a single chunk of content information.
-        It is used everytime we have to display it as a whole page.
-        """
-    
-    
-    def htmlList(self, ):
-        """
-        This view renders a single chunk of content information in a LIST.
-        We must use that everytime we return a long and condensed list of information.
-        """
-    
-    def htmlSummary(self, ):
-        """
-        We use this to display a content summary, for example inside the wall.
-        Every content type may generate its own summary.
-        """
-        # TODO: Check if content defines its own summary view
-        # XXX TODO
-    
-        # Special HTML renderers for various elements
-        t = get_template('content/summary.part.html')
-        return t.render(Context({'content': self.content}))
 
 
