a4c11d25ce5b1ec140fc8287e7a20e9b9961cea6
pjgrizel <pjgrizel@54d5ac7e-47a4-472c-8442-392f3838c228>
2010-09-21 11:06:59 +0000

    moved modles
    git-svn-id: https://svn.liances.com/svnroot/projects/TwistraNet/trunk@4100 54d5ac7e-47a4-472c-8442-392f3838c228

--- twistranet/models.py
+++ twistranet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# from models import *
+
+class Account(models.Model):
+    pass
+    
---
+++ twistranet/tn_models/__init__.py
@@ -0,0 +1,11 @@
+# Importing all models from submodules
+
+__all__ = (
+    # Account stuff
+    "Account",
+)
+
+from accountmodels import *
+from contentmodels import *
+from relationmodels import *
+from statusupdatemodels import *
