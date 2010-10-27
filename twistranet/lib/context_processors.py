845c20555e410176ef853080c24a9e5ebcb29e70
pjgrizel <pjgrizel@54d5ac7e-47a4-472c-8442-392f3838c228>
2010-10-27 15:43:31 +0000

    The usual forgotten file
    git-svn-id: https://svn.liances.com/svnroot/projects/TwistraNet/trunk@4353 54d5ac7e-47a4-472c-8442-392f3838c228

---
+++ twistranet/lib/context_processors.py
@@ -0,0 +1,18 @@
+"""
+Special context processor to handle the logged_account variable for all of TN calls.
+"""
+
+def security_context(request):
+    """
+    Retrieve the logged account and populate the variable (don't do anything with anon account by now)
+    """
+    if request.user:
+        return (
+            {
+                'logged_account': request.user.get_profile(),
+            }
+        )
+
+    # No user logged-in, we return an empty tuple, we've got nothing to populate.
+    return ()
+    
