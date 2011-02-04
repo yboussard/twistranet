c78fab5d3248a4a99e52238fe2c5b2c9950aed4f
Pierre-Julien Grizel <pjgrizel@numericube.com>
2011-02-04 13:25:48 +0100

    deactivated the sorl-tmbl patch

--- twistranet/core/patches/sort_thumbnail.py
+++ twistranet/core/patches/sort_thumbnail.py
@@ -9,8 +9,8 @@ def absolute_url(self):
     return thumbnail absolute_url
     """
     base_url = settings.BASE_URL
-    return '%s%s' %(base_url, self.storage.url(self.name))
+    return '%s--%s' %(base_url, self.storage.url(self.name))
 
 log.info("Patched sorl.thumbnail.image.ImageFile.url to get absolute url")
 ImageFile.old_url = ImageFile.url
-ImageFile.url = absolute_url
+#ImageFile.url = absolute_url
