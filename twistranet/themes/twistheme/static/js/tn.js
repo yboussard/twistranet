5469e3760271dbae9c6ca15c8995abad89eb0438
macadames <jeanmat.grimaldi@gmail.com>
2011-02-14 15:50:28 +0100

    code simplified for inputs Hints (fix also bug with hidden fields)

--- twistranet/themes/twistheme/static/js/tn.js
+++ twistranet/themes/twistheme/static/js/tn.js
@@ -275,8 +275,8 @@ gridOnSelect = function(grid) {
 
 
 // TODO in future : remove cache: true
-// and improve ajax request with data 
-// for multiple uploaders in a same page 
+// and improve ajax request with data (type = image etc ... see ticket #1196,
+// and uploaderid=toto for multiple uploaders in a same page )
 loadQuickUpload = function(obj) {
     var uploadUrl = home_url + 'resource_quickupload/' ;
     var tnUploader = jq(obj);
@@ -341,6 +341,7 @@ var twistranet = {
         twistranet.tnGridActions();
         twistranet.formUndo(); 
         twistranet.formProtection();
+        twistranet.formInputsHints();
         twistranet.loadUploaders();
         twistranet.initWysiwygBrowser();
     },
@@ -496,6 +497,15 @@ var twistranet = {
             })
         }
     },
+    formInputsHints : function(e) {
+        var inputs_with_hints = 'input[type="text"], input[type="checkbox"], input[type="radio"], select, textarea';
+        jq(inputs_with_hints).focusin(function(e) {
+            jq('.hint', jq(this).parent()).show();
+        });
+        jq(inputs_with_hints).focusout(function(e) {
+            jq('.hint', jq(this).parent()).hide();
+        });
+    },
     tnGridActions: function(e) {
      jq('.tnGrid').each(function(){
             gridOnSelect(this);
@@ -529,55 +539,5 @@ var twistranet = {
         // put here the code for other editors (ckeditor ....)
     }
 }
-
-function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function() {
-      oldonload();
-      func();
-    }
-  }
-}
-
-function prepareInputsForHints() {
-  var inputs = document.getElementsByTagName("input");
-  for (var i=0; i<inputs.length; i++){
-    inputs[i].onfocus = function () {
-      this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
-    }
-    inputs[i].onblur = function () {
-      this.parentNode.getElementsByTagName("span")[0].style.display = "none";
-    }
-  }
-  var selects = document.getElementsByTagName("select");
-  for (var k=0; k<selects.length; k++){
-    if (selects[k].parentNode.getElementsByTagName("span")[0]) {
-      selects[k].onfocus = function () {
-        this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
-      }
-      selects[k].onblur = function () {
-        this.parentNode.getElementsByTagName("span")[0].style.display = "none";
-      }
-    }
-  }
-  var selects = document.getElementsByTagName("textarea");
-  for (var k=0; k<selects.length; k++){
-    if (selects[k].parentNode.getElementsByTagName("span")[0]) {
-      selects[k].onfocus = function () {
-        this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
-      }
-      selects[k].onblur = function () {
-        this.parentNode.getElementsByTagName("span")[0].style.display = "none";
-      }
-    }
-  }
-  
-}
-addLoadEvent(prepareInputsForHints);
-
-
 
 jq(document).ready(twistranet.__init__)
